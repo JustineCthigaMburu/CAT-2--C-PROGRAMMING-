530519d8391cb5b9c0a4133292a8331d5da0d1d9
JustineCthigaMburu <cyprianmburu@gmail.com>
2024-11-07 17:24:25 +0300

    Create CAT 2_Q2.C.

---
+++ CAT 2_Q2.C
@@ -0,0 +1,17 @@
+/* AUTHOR: MBURU C JUSTINE THIGA
+   REGISTRATION NUMBER: CT101/G/21512/24
+   DATE: 07/11/2024
+*/
+// preprocessing directives scanf (),printf ()
+#include<stdio.h>
+int main (){
+int i, j;
+int goals [2][4] ={{65,92,84,72},{35,70,59,67}};
+      for(i=0;i<2;i++){
+      for(j=0;j<4;j++){
+      
+printf("goals[%d][%d]= %d\n",i,j ,goals[i] [j]); 
+}
+}
+return 0;
+}
